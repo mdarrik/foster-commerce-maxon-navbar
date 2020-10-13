853a69ab6b9034a03545387f3b64b42f1955e010
Darrik <30670444+mdarrik@users.noreply.github.com>
2020-10-12 22:25:38 -0700

    feat: adds vue-fragment

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { Plugin } from "vue-fragment";
+// use vue-fragment so nothing has to be wrapped in a div
+Vue.use(Plugin);
 
 Vue.config.productionTip = false;
 

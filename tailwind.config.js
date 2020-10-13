25a841ce6bdf1312163b9f9cc832b96b82dc2518
Darrik <30670444+mdarrik@users.noreply.github.com>
2020-10-12 22:05:53 -0700

    feat: adds tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  /**Turns on future utils to make migration easier */
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+    defaultLineHeights: true,
+    standardFontWeights: true
+  },
+  purge: [],
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}

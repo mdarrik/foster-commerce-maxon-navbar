835828a190e9ff49725b413902bfcbd9c063b78b
Darrik <30670444+mdarrik@users.noreply.github.com>
2020-10-14 04:28:33 +0000

    feat: add tailwind to Vue app

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,10 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="postcss">
+@tailwind base;
+
+@tailwind components;
+
+@tailwind utilities;
 </style>

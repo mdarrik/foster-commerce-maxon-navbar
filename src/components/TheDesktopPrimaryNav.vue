a4615b7263996c2e21f04d718e9bfbfb4a69e431
Darrik <30670444+mdarrik@users.noreply.github.com>
2020-10-18 23:20:37 +0000

    doc: documents the component

--- src/components/TheDesktopPrimaryNav.vue
+++ src/components/TheDesktopPrimaryNav.vue
@@ -31,18 +31,30 @@
 </template>
 <script>
 import DropdownMenu from "./DropdownMenu";
+/**The primary set of nav options for the site on large screens.
+ * Also manages the state of any child dropdown elements. */
 export default {
-  /** */
-  props: { categories: Array },
+  props: {
+    /**The array of categories that make up the nav items. if the category is a dropdown,
+     * needs to have a subcategories property.
+     * Otherwise it needs to have a url for the link to target.
+     */
+    categories: Array
+  },
   components: {
     DropdownMenu
   },
   data() {
     return {
+      /**A numerical index to keep track of which dropdown element is open
+       * If no dropdowns are open, assign to null
+       */
       openListIndex: null
     };
   },
   methods: {
+    /**method that fires when a dropdown button is clicked.
+     * Handles changing the active openListIndex appropriately */
     dropdownButtonClicked(index) {
       if (this.openListIndex === index) {
         this.openListIndex = null;
@@ -50,8 +62,12 @@ export default {
         this.openListIndex = index;
       }
     },
+    /**Event that fires when any child element loses focus.
+     * This can be used to make sure that any open dropdowns are closed when the list loses focus*/
     focusOut(event) {
       const listRef = this.$refs.listRef;
+      //if a nav is open, and the list element doesn't contain the
+      //target of the next focus, unset the openListIndex to close the open nav.
       if (this.openListIndex && !listRef.contains(event.relatedTarget)) {
         this.openListIndex = null;
       }

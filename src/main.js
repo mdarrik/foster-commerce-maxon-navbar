1b6d862712faecc991a3d83e5614d3be0b84b9be
Darrik <30670444+mdarrik@users.noreply.github.com>
2020-10-19 07:52:59 +0000

    adds inert polyfill
    This polyfill makes overlays & modals more accessible

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import { Plugin } from "vue-fragment";
+// import wicg-inert so the inert attribute can be used
+// inert provides some QOL stuff for modals & overlays. Useful for search input + hamburger menu
+import "wicg-inert";
 // use vue-fragment so nothing has to be wrapped in a div
 Vue.use(Plugin);
 
